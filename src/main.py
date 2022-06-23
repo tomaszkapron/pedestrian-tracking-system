868f6b3f9d987b6e36faa7e74e3affafe6ad0613
jwietrzykowski <jan.m.wietrzykowski@gmail.com>
2022-06-23 19:42:16 +0200

    Komentarze i ocena.

--- src/main.py
+++ src/main.py
@@ -7,6 +7,25 @@ from src.FeatureMatcher import FeatureMatcher
 from src.HistogramMatcher import HistMatcher
 from src.GraphModel import GraphModel
 
+# TODO Jakość kodu i raport (5/5)
+# TODO Raport przejrzysty i wyczerpujący.
+# TODO Kod przejrzysty i dobrze udokumentowany.
+
+# TODO Skuteczność śledzenia 0.0 (0/5)
+# TODO [0.00, 0.50) - 0.0
+# TODO [0.50, 0.55) - 0.5
+# TODO [0.55, 0.60) - 1.0
+# TODO [0.60, 0.65) - 1.5
+# TODO [0.65, 0.70) - 2.0
+# TODO [0.70, 0.75) - 2.5
+# TODO [0.75, 0.80) - 3.0
+# TODO [0.80, 0.85) - 3.5
+# TODO [0.85, 0.90) - 4.0
+# TODO [0.90, 0.95) - 4.5
+# TODO [0.95, 1.00) - 5.0
+
+# stderr:
+# python3: can't open file 'main.py': [Errno 2] No such file or directory
 
 def processData():
     parser = argparse.ArgumentParser()
